9255eb9b1e3754203754acb55d66f3deba446e2f
Jaspreet Singh Dhami <jdhami1@ualberta.ca>
2023-09-29 10:45:39 -0600

    this is the skeleton of the code

--- server.py
+++ server.py
@@ -33,6 +33,65 @@ class MyWebServer(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
         self.request.sendall(bytearray("OK",'utf-8'))
+        print(f"The type of the data received is {type(self.data)}")
+        self.data = self.data.decode().strip()
+        print(f"Now the updated type of data is {type(self.data)} and the data itself is:\n {self.data}\n\n")
+        print("---------------------------")
+
+        components_list = self.data.split() # This is going to split the data received into its different components which we can assess as we go.
+        for comp in components_list:
+            print(comp)
+
+        # Now we are going to check if the path provided is a valid path 
+        request_type = components_list[0]   # We are only handling GET requests
+        requested_path = components_list[1]   # Since path is our second element in the list of the components
+        http_ver = components_list[2]   # Since http version is the third element in the list of components
+        folder = "/www"         # Since as a webserver, we only want to serve files that are in our folder www
+
+        if requested_path[-1] == '/':
+        # We are going to open index.html by default if the user has entered the directory somehow
+            requested_path += 'index.html'
+
+        full_path = folder + requested_path # This is the path that we will eventually go finding
+
+        # Now we are going to check the following parts of the request as per the requirements:
+        # 1. If the request method is GET or not
+        # 2. If the request has requested a valid path
+        # 3. If we have the requested file or not
+ 
+        if request_type == 'GET':
+        # We will not be handling any other request types other than GET
+            pass
+            try:
+                file = open(full_path)
+                content = file.read()
+                self.handle200(full_path,content)   # Path and File both Valid, 200 OK
+            
+            except FileNotFoundError:
+                self.handle404()
+                pass
+            except IsADirectoryError:
+                self.handle301()
+                pass
+        else:
+            self.handle405()
+    
+    # These are the status code handlers
+    def handle200(self, path, content):
+    # This function will handle 200 status code case
+        pass
+
+    def handle301(self):
+    # This function will handle 301 status code case
+        pass
+
+    def handle404(self):
+    # This function will handle 404 status code case
+        pass
+
+    def handle405(self):
+    # This function will handle 405 status code case
+        pass
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -40,6 +99,7 @@ if __name__ == "__main__":
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
+    
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
